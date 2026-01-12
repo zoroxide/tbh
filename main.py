72560884003f771d5388e35d57a77b6e7ec240f5
Loay <loay@loay.space>
2026-01-12 17:00:02 +0200

    Change FastAPI server port from 8000 to 4000

--- main.py
+++ main.py
@@ -76,4 +76,4 @@ async def search(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=4000, workers=1)
